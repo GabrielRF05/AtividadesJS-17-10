40dcff1ffe6fe9627b6a9c021485b92ce5287db6
GabrielRF05 <gabrieldosreis512@gmail.com>
2024-10-17 15:35:06 -0300

    Add files via upload

---
+++ 171024/script.js
@@ -0,0 +1,13 @@
+let n1 = parseInt(prompt("Insira o primeiro numero"))
+let n2 = parseInt(prompt("Insira o segundo numero"))
+let n3 = parseInt(prompt("Insira o terceiro numero"))
+let res
+
+function media(n1, n2, n3){
+
+res = (n1+n2+n3)/3
+return res
+}
+console.log(media(n1,n2,n3))
+
+
